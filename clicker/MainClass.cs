acac7d277a3545a4e988aa84a571e77c70edc1d7
Sapfir0 <sapfir999999@yandex.ru>
2019-12-01 00:53:14 +0400

    first commit

---
+++ clicker/MainClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace clicker {
+    class MainClass {
+        static int counter;
+        static int multiplier;
+        Button clickBtn;
+        TextView countPoints;
+
+
+        public MainClass(Button clickBtn, TextView countPoints) {
+            this.clickBtn = clickBtn;
+            this.countPoints = countPoints;
+            clickBtn.Click += AddOneToCounter;
+        }
+
+        public void AddOneToCounter(object sender, EventArgs e) {
+            counter++;
+            countPoints.Text = counter.ToString();
+        }
+    }
+
+
+}
