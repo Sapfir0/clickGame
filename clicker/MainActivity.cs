26fd94b8205c13cf0e7e0d30c59242c27a94ac36
Sapfir0 <sapfir999999@yandex.ru>
2019-12-02 22:07:11 +0400

    черт, я не успел закоммить

--- clicker/MainActivity.cs
+++ clicker/MainActivity.cs
@@ -16,12 +16,10 @@ namespace clicker
 
         Button clickBtn;
         TextView countPoints;
-        Dictionary<int, int> MultiplyerCosts = new Dictionary<int, int> {
-            [Resource.Id.lowMultiplyer] = 10,
-            [Resource.Id.mediumMultiplyer] = 30
-        };
+
         MainClass main;
         Game game;
+        Shop shop;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -35,21 +33,17 @@ namespace clicker
 
             clickBtn = FindViewById<Button>(Resource.Id.clickBtn);
             clickBtn.Click += AddOneToCounterListener;
-
             countPoints = FindViewById<TextView>(Resource.Id.countPoints);
 
 
-            var setX2multiplyerBtn = FindViewById<Button>(Resource.Id.lowMultiplyer);  // TODO вынести это в шоп, сделать его фабрикой
-            setX2multiplyerBtn.Enabled = false;
-            setX2multiplyerBtn.Text = "L " + MultiplyerCosts[Resource.Id.lowMultiplyer];
-            setX2multiplyerBtn.Click += SetLowModifier;
-
-            var setX3multiplyerBtn = FindViewById<Button>(Resource.Id.mediumMultiplyer);
-            setX3multiplyerBtn.Enabled = false;
-            setX3multiplyerBtn.Text = "M " + MultiplyerCosts[Resource.Id.mediumMultiplyer];
-            setX3multiplyerBtn.Click += SetMediumModifier;
             main = new MainClass(); 
             game = new Game(main);
+            shop = new Shop(main);
+
+            var tableLayout = FindViewById<TableLayout>(Resource.Id.tableLayout);
+            shop.CreateButtonOnNewRow(this, ref tableLayout, 10);
+            shop.CreateButtonOnNewRow(this, ref tableLayout, 30);
+
 
             var startIdleBtn = FindViewById<Button>(Resource.Id.idleStart);
             startIdleBtn.Click += game.StartIdleFarm;
@@ -62,7 +56,7 @@ namespace clicker
             Console.WriteLine(points);
             string intSequence = points.ToString();
             countPoints.Text = intSequence;
-            foreach (KeyValuePair<int, int> buttonCost in MultiplyerCosts) { // можно будет пропускать те, которые мы прошли давно, и не обходить каждый раз их
+            foreach (KeyValuePair<int, int> buttonCost in shop.MultiplyerCosts) { // можно будет пропускать те, которые мы прошли давно, и не обходить каждый раз их
                 using (var h = new Handler(Looper.MainLooper))
                     h.Post(() => {
                         var openingButton = FindViewById<Button>(buttonCost.Key);
@@ -75,19 +69,6 @@ namespace clicker
             main.AddMultipierPointsToCounter();
         }
 
-        private void SetMediumModifier(object sender, EventArgs e) {
-            main.DecrementCurrentPoints(MultiplyerCosts[Resource.Id.mediumMultiplyer]);
-
-            double modifier = 2;
-            MainClass.IncrementMultiplier(modifier);
-        }
-
-
-        private void SetLowModifier(object sender, EventArgs e) {
-            main.DecrementCurrentPoints(MultiplyerCosts[Resource.Id.lowMultiplyer]);
-            double modifier = 1;
-            MainClass.IncrementMultiplier(modifier);
-        }
 
 
 
--- clicker/Shop.cs
+++ clicker/Shop.cs
@@ -12,7 +12,56 @@ using Android.Widget;
 
 namespace clicker {
     class Shop {
+        public Dictionary<int, int> MultiplyerCosts = new Dictionary<int, int> {
+
+        };
+
+        class multiplyer {
+
+        }
+
+        MainClass main;
+        public Shop(MainClass main) {
+            this.main = main;
+        }
+
+        private Button CreateButton(Context context, string buttonText, int multiplyer) {
+            var cell1 = new Button(context);
+            cell1.Text = buttonText + multiplyer.ToString();
+            cell1.Click +=  BuyModifier;
+            return cell1;
+        }
+
+        public void CreateButtonOnNewRow(Context context, ref TableLayout tableLayout, int multiplyer, string buttonText =null) {
+            var tableRow1 = new TableRow(context);
+            var newBtn = CreateButton(context, buttonText, multiplyer);
+            var rand = new Random();
+            var randomId = rand.Next(1000000000, 2099999999);
+            while (MultiplyerCosts.ContainsKey(randomId)) { //надеюсь я правильно написал условие
+                randomId = rand.Next(1000000000, 2099999999);
+                newBtn.Id = randomId;
+            }
+            tableRow1.AddView(newBtn);
+            tableLayout.AddView(tableRow1);
+
+            MultiplyerCosts.Add(newBtn.Id, multiplyer);
+        }
 
 
+       
+        public void CreateButtonOnExistingRow(Context context, ref TableRow tableRow, ref TableLayout tableLayout, int multiplyer, string buttonText = null) {
+            var newBtn = CreateButton(context, buttonText, multiplyer);
+            tableRow.AddView(newBtn);
+            tableLayout.AddView(tableRow);
+
+            MultiplyerCosts.Add(newBtn.Id, multiplyer);
+        }
+
+        private void BuyModifier(object sender, EventArgs e) {
+            var currentBtn = (Button)sender;
+            var multiplyerCost = MultiplyerCosts[currentBtn.Id];
+            main.DecrementCurrentPoints(multiplyerCost);
+            MainClass.IncrementMultiplier(multiplyerCost);
+        }
     }
 }
